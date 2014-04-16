a643efe2002dc7bc374e604ff4b9417c70d71d9a
Daniel Weinmann <danielweinmann@gmail.com>
2014-04-16 11:41:56 -0300

    Adding access token to FB integration

--- app/models/event.rb
+++ app/models/event.rb
@@ -22,26 +22,29 @@ class Event < ActiveRecord::Base
     self.url.match(FB_REGEX)[1]
   end
   memoize :facebook_id
+
+  def facebook_data
+    return nil unless self.url.present? and self.url.match(FB_REGEX)
+    access_token = HTTParty.get("https://graph.facebook.com/oauth/access_token?client_id=#{ENV['FACEBOOK_APP_ID']}&client_secret=#{ENV['FACEBOOK_APP_SECRET']}&grant_type=client_credentials").body.match(/access_token=(.+)/)[1]
+    data = JSON.parse(HTTParty.get("https://graph.facebook.com/#{self.facebook_id}?access_token=#{URI::escape(access_token)}").body)
+    return nil unless data["name"]
+    return nil unless data["start_time"]
+    return nil unless data["location"]
+    data
+  end
+  memoize :facebook_data
   
   private
   
   def must_have_facebook_data
-    raise unless self.url.present? and self.url.match(FB_REGEX)
-    data = JSON.parse(HTTParty.get("https://graph.facebook.com/#{self.facebook_id}").body)
-    raise unless data["name"]
-    raise unless data["start_time"]
-    raise unless data["end_time"]
-    raise unless data["location"]
-  rescue
-    errors.add(:url, "não é um evento aberto do Facebook")
+    errors.add(:url, "não é um evento aberto do Facebook") unless self.facebook_data
   end
   
   def get_facebook_data
-    data = JSON.parse(HTTParty.get("https://graph.facebook.com/#{self.facebook_id}").body)
-    self.name = data["name"] unless self.name.present?
-    self.start_time = data["start_time"] unless self.start_time.present?
-    self.end_time = data["end_time"] unless self.end_time.present?
-    self.location = data["location"] unless self.location.present?
+    self.name = self.facebook_data["name"] unless self.name.present?
+    self.start_time = self.facebook_data["start_time"] unless self.start_time.present?
+    self.end_time = self.facebook_data["end_time"] unless self.end_time.present?
+    self.location = self.facebook_data["location"] unless self.location.present?
     self.picture = HTTParty.get("https://graph.facebook.com/#{self.facebook_id}/picture?type=large", follow_redirects: false).headers["location"] unless self.picture.present?
   end
   
