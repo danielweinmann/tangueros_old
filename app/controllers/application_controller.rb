40a6d1706a333e839128de4880c575fa1230b7fb
Daniel Weinmann <danielweinmann@gmail.com>
2014-05-06 02:43:52 -0300

    Enabling devise to edit user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :namespace, :display_headline_and_article!, :hide_headline_and_article!, :display_headline_and_article?
 
   def display_headline_and_article?
@@ -20,6 +20,11 @@ class ApplicationController < ActionController::Base
     names = self.class.to_s.split('::')
     return "null" if names.length < 2
     names[0..(names.length-2)].map(&:downcase).join('_')
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
 
 end
